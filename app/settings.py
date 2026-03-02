ed47ba4e9a12709862c59be2ecdb0a1cfb69f7bd
Cypha <omarcypha@gmail.com>
2026-03-02 17:37:11 +0000

    update

--- app/settings.py
+++ app/settings.py
@@ -165,6 +165,6 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://examination-attendance.vercel.app",
+    "https://examination-attendance.vercel.app/",
 ]
 CORS_ALLOW_CREDENTIALS = True
