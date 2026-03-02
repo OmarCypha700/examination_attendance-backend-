531d8ae8f395b146725fa82e70cb08db9d673cba
Cypha <omarcypha@gmail.com>
2026-03-02 14:59:00 +0000

    update static

--- app/settings.py
+++ app/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 AUTH_USER_MODEL = "accounts.User"
